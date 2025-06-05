28307be1d8acf1a3e6d026c16c2a4f7f5c7d6cb2
laboratorio <laboratorio@UPOAULA10714>
2025-06-05 07:19:42 -0500

    Agregar archivos de proyecto.

---
+++ RodriguezJExamen2doProgreso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RodriguezJExamen2doProgreso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
